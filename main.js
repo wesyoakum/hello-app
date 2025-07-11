7915b64a6cf375fa6f96290033ec529a3670c949
wesyoakum <wesyoakum@gmail.com>
2025-07-11 07:40:19 -0500

    make speeds dashed

--- main.js
+++ main.js
@@ -537,9 +537,9 @@ function renderCharts(depths, tension, availTension, actualSpeed, rpmSpeed, powe
     data: {
       labels: depths,
       datasets: [
-        { label: 'Available Speed (m/min)', data: actualSpeed, borderColor: '#5c82a4', fill: false },
-        { label: 'RPM Limited Speed (m/min)', data: rpmSpeed, borderColor: '#6d4688', fill: false },
-        { label: 'Power Limited Speed (m/min)', data: powerSpeed, borderColor: '#65c98f', fill: false },
+        { label: 'Available Speed (m/min)', data: actualSpeed, borderColor: '#5c82a4', fill: false, pointRadius: 0  },
+        { label: 'RPM Limited Speed (m/min)', data: rpmSpeed, borderColor: '#6d4688', borderDash: [5,5], fill: false, pointRadius: 0  },
+        { label: 'Power Limited Speed (m/min)', data: powerSpeed, borderColor: '#65c98f', borderDash: [5,5], fill: false, pointRadius: 0  },
         { label: 'Required Speed', data: depths.map(() => reqSpeed), borderColor: 'gray', borderDash: [5,5], fill: false, pointRadius: 0 }
       ]
     },
